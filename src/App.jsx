bf200a43ae2faf383a76c3c151cc3faa84386c4a
Nukumizu <y_ikebe@imin.co.jp>
2022-06-07 18:43:54 +0900

    Learn Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
 function App() {
+  const profiles = [
+    { name: 'taro', age: 10 },
+    { name: 'Hanako', age: 5 }
+  ]
   return (
-    <div className="App">
-      
+    <div className='App'>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
     </div>
   );
+}
+
+function User(props) {
+  return <div>I am {props.name}, and {props.age} years old1</div>;
 }
 
 export default App;
